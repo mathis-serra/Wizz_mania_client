ed01d54552d53e17eb2bc3b4f19bf6e314bf2071
Dhjali <barbara.vanni@laplateforme.io>
2024-11-15 11:58:23 +0100

    [INITIAL] SFML window open

--- Client_2.h
+++ Client_2.h
@@ -1,26 +1,29 @@
- #ifndef CLIENT_2_H
- #define CLIENT_2_H
+#ifndef CLIENT_2_H
+#define CLIENT_2_H
 
- #include <sys/socket.h>
- #include <netinet/in.h>
- #include <arpa/inet.h>
- #include <unistd.h>
- #include <string>
- #include <thread>
- #include <iostream>
+#include <ws2tcpip.h>
+#include <string>
+#include <thread>
+#include <iostream>
 
- class Client_2 {
- public:
-     Client_2(const std::string &ipAddress, int port);
-     ~Client_2();
-     bool connectToServer();
-     void sendMessage();
-     void receiveMessage();
- private:
-     std::string ipAddress;
-     int port;
-     int clientSocket;  // Changed from SOCKET to int
-     std::thread receiveThread;
- };
+#pragma comment(lib, "ws2_32.lib")
 
- #endif
+class Client_2 {
+public:
+    Client_2(const std::string &ipAddress, int port);
+    ~Client_2();
+    bool connectToServer();
+    // void sendMessage();
+    void receiveMessage();
+
+    void sendMessage(const std::string & string);
+
+private:
+    std::string ipAddress;
+    int port;
+    SOCKET clientSocket;
+    WSADATA wsaData;
+    std::thread receiveThread;
+};
+
+#endif
